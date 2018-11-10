51a3ebbdf71cf798a8259cb74d1fe5f923b1491e
1115091188@qq.com <123456sgb>
2018-11-10 22:55:05 +0800

    common 分布式id提交

---
+++ shopcommon/src/main/java/com/lza/PageResult.java
@@ -0,0 +1,30 @@
+package com.lza;
+
+import java.util.List;
+
+public class PageResult <T> {
+    private  long total;
+
+    public PageResult(long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+
+    private List<T> rows;
+}
